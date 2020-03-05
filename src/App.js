3812aa1299ad9894cb069e0fb0a2eb1542706871
Dominic Chu <dominic.m.chu@gmail.com>
2020-03-05 11:39:59 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+// use this syntax. 
+import { Button } from 'semantic-ui-react'; 
+
+
+function App() {
+  return (
+    <div>
+      JUST STARTING
+    </div>
+  );
+}
+
+export default App;
