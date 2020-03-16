2b17420398b65557d2ec429357206347e9b3ac7c
domitalk <dominic.m.chu@gmail.com>
2020-03-16 12:08:40 -0400

    filldata for projects

--- src/fillData/projectData.js
+++ src/fillData/projectData.js
@@ -1,12 +1,12 @@
 const projectData = [
     {
-        "imageUrl": "", 
-        "header": "Header Example", 
-        "description": "Description Example", 
+        "imageUrl": "https://res.cloudinary.com/dwazq8zps/image/upload/v1583940185/rbkhzipvosexmgvcxswh.jpg", 
+        "header": "At Anon", 
+        "description": "Image hosting and sharing application", 
         "demoLink": "",
-        "gitFront": "",
-        "gitBack": "", 
-        "liveSite": ""
+        "gitFront": "https://github.com/Domitalk/atanon",
+        "gitBack": "https://github.com/Domitalk/atanon-api", 
+        "liveSite": "www.domitalk.github.io/atanon"
     },
     {
         "imageUrl": "", 
