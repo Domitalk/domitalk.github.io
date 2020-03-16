db59686c22d94cbdf7dd1934be4e3298f2a95197
domitalk <dominic.m.chu@gmail.com>
2020-03-16 12:25:25 -0400

    projects layout

--- src/Components/Project.js
+++ src/Components/Project.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Card from 'react-bootstrap/Card'
+import Button from 'react-bootstrap/Button'
 
 // {
 //     "imageUrl": "", 
@@ -13,6 +14,25 @@ import Card from 'react-bootstrap/Card'
 
 function Project(props) {
 
+    const makeButton = (linkVar, desc) => {
+        return (
+            <Button href={linkVar} target="_blank" variant="outline-primary" className="mx-1 my-1">
+                {desc}
+            </Button>
+        )
+    }
+
+    const createButtons = () => {
+        return (
+            <>
+                {(props.project.demoLink.length > 1) ? makeButton(props.project.demoLink, "Demo") : null}
+                {(props.project.gitFront.length > 1) ? makeButton(props.project.gitFront, "Github Front") : null}
+                {(props.project.gitBack.length > 1) ? makeButton(props.project.gitBack, "Github Back") : null}
+                {(props.project.liveSite.length > 1) ? makeButton(props.project.liveSite, "Live Site") : null}
+            </>
+        )
+    }
+
     return (
         <Card className="mx-2 my-2">
             <Card.Header>{props.project.header}</Card.Header>
@@ -21,6 +41,7 @@ function Project(props) {
                 <Card.Text>
                     {props.project.description}
                 </Card.Text>
+                {createButtons()}
             </Card.Body>
         </Card>
     )
--- src/fillData/projectData.js
+++ src/fillData/projectData.js
@@ -2,11 +2,11 @@ const projectData = [
     {
         "imageUrl": "https://res.cloudinary.com/dwazq8zps/image/upload/v1583940185/rbkhzipvosexmgvcxswh.jpg", 
         "header": "At Anon", 
-        "description": "Image hosting and sharing application", 
+        "description": "Image hosting and sharing application. Created using React and Material UI on the frontend and Ruby on Rails for the backend. Hosted on Heroku, Github and images hosted on Cloudinary.", 
         "demoLink": "",
         "gitFront": "https://github.com/Domitalk/atanon",
         "gitBack": "https://github.com/Domitalk/atanon-api", 
-        "liveSite": "www.domitalk.github.io/atanon"
+        "liveSite": "https://domitalk.github.io/atanon"
     },
     {
         "imageUrl": "", 
