30c7b1319025e8be19bac7d682e2506149978d0e
domitalk <dominic.m.chu@gmail.com>
2020-09-28 16:25:37 -0400

    feed data

--- src/fillData/blogData.js
+++ src/fillData/blogData.js
@@ -28,17 +28,48 @@ const blogData = [
         "gitBack": "", 
         "liveSite": "",
         "bloglink": "https://medium.com/better-programming/infinite-scrolling-in-react-839bbade153f?source=friends_link&sk=e72657815ec6db5f009da92c7d1c2aae"
-    } ,
+    },
     {
-        "imageUrl": "https://miro.medium.com/max/1400/1*5Kn53lcXoZHqyCENBG6_Cg.jpeg", 
-        "header": "How to Build Infinite Scroll in React", 
-        "description": "Building out a Infinite Scroll in React without any libraries so you can learn what is happening with the listeners. Published on Better Programming.", 
+        "imageUrl": "", 
+        "header": "Async Arrow Functions and Redux", 
+        "description": "How modern JavaScript syntax and Redux combine for flawless asynchronus functionality.", 
         "demoLink": "",
         "gitFront": "",
         "gitBack": "", 
         "liveSite": "",
-        "bloglink": "https://medium.com/better-programming/infinite-scrolling-in-react-839bbade153f?source=friends_link&sk=e72657815ec6db5f009da92c7d1c2aae"
-    } 
+        "bloglink": "https://medium.com/@dominic.m.chu/async-arrow-functions-and-redux-ed41ea06c1a6?sk=26feea7eefedd66d8984e0059d3f49fc"
+    },
+    {
+        "imageUrl": "https://www.manhattanmobile.com/wp-content/uploads/2018/08/react-native-workshop-1024x538.jpg", 
+        "header": "Trying React-Native", 
+        "description": "Basics of React-Native and the differences between React and Native.", 
+        "demoLink": "",
+        "gitFront": "",
+        "gitBack": "", 
+        "liveSite": "",
+        "bloglink": "https://medium.com/@dominic.m.chu/trying-react-native-b2a1bb3d1574"
+    },
+    {
+        "imageUrl": "", 
+        "header": "HexaDecimal Conversions", 
+        "description": "Converting colors to Hexadecimal values.", 
+        "demoLink": "",
+        "gitFront": "",
+        "gitBack": "", 
+        "liveSite": "",
+        "bloglink": "https://medium.com/@dominic.m.chu/hexadecimal-conversions-e9494b3f4604?source=friends_link&sk=ab033445a51408aa7899b78db57e7b01"
+    },
 ]
 
 export default blogData
+
+// {
+//     "imageUrl": "", 
+//     "header": "Header Example", 
+//     "description": "Description Example", 
+//     "demoLink": "",
+//     "gitFront": "",
+//     "gitBack": "", 
+//     "liveSite": "",
+//     "bloglink": ""
+// },
