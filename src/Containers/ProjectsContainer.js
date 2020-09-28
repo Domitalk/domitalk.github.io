6095ada9f5d009891fd790a21f466c6b39e80351
domitalk <dominic.m.chu@gmail.com>
2020-09-28 16:01:19 -0400

    cleanup

--- src/Containers/ProjectsContainer.js
+++ src/Containers/ProjectsContainer.js
@@ -4,8 +4,6 @@ import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 import Button from 'react-bootstrap/Button'
 import Project from '../Components/Project'
-import projectData from '../fillData/projectData.js'
-// import Card from 'react-bootstrap/Card'
 
 function ProjectsContainer(props) {
     const [projectsToShow, setProjectsToShow] = useState([])
@@ -54,15 +52,8 @@ function ProjectsContainer(props) {
             <Row> 
                 <Col xs={{spand:12, order: 1}} md={{ span: 12, order: 1}} lg={{ span:12, order: 1}}>
                     <h3 className="text-center">{props.header}</h3>
-
-                    {/* <Card >
-                        <Card.Text as="h3" className="text-center">Recent Projects</Card.Text>
-                    </Card> */}
                 </Col>
             </Row>
-            {/* <Row>
-                <div className="empty"/>
-            </Row> */}
             <Row>
                 {mapAllProjects()}
             </Row>
@@ -77,54 +68,3 @@ function ProjectsContainer(props) {
 }
 
 export default ProjectsContainer; 
-
-
-
-
-
-// import React from 'react'
-// import Container from 'react-bootstrap/Container'
-// import Row from 'react-bootstrap/Row'
-// import Col from 'react-bootstrap/Col'
-// import Project from '../Components/Project'
-// import projectData from '../fillData/projectData.js'
-// import Card from 'react-bootstrap/Card'
-
-
-
-
-// function ProjectsContainer() {
-
-//     const mapAllProjects = () => {
-//         return projectData.map((project) => {
-//             return (
-//                 <Col xs={12} md={12} lg={12}>
-//                     <Project project={project}/>
-//                 </Col>
-//             )
-//         })
-//     }
-
-//     return (
-//         <Container id="Projects">
-//             <Row>
-//                 <div className="empty"/>
-//             </Row>
-//             <Row> 
-//                 <Col xs={{spand:12, order: 1}} md={{ span: 12, order: 1}} lg={{ span:12, order: 1}}>
-//                     <Card border="secondary">
-//                         <Card.Text className="text-center">Recent Projects</Card.Text>
-//                     </Card>
-//                 </Col>
-//             </Row>
-//             <Row>
-//                 <div className="empty"/>
-//             </Row>
-//             <Row>
-//                 {mapAllProjects()}
-//             </Row>
-//         </Container>
-//     )
-// }
-
-// export default ProjectsContainer; 
